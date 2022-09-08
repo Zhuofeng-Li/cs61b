eb38dd7e7ed038d444791949476102dca36d6ba5
being <89558990+being12345@users.noreply.github.com>
2022-09-08 17:53:21 +0800

    lab1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -188,7 +188,7 @@ public class ArrayDeque<B> {
         }
         else{
             int m = items.length - 1 - before;
-            if(m >= index + 1){//数与个数的关系
+            if(m >= index + 1){//数与个数的关系,这里有等于
                 return items[before + index + 1];}
             else{
                 return items[index - m];
